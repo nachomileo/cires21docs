32dfee70cd57977a8b1e1725ba9adb55767d30ba
Nacho Mileo <35810074+nachomileo@users.noreply.github.com>
2023-11-23 11:34:30 +0100

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -2,9 +2,9 @@
 
 # -- Project information
 
-project = 'Lumache'
-copyright = '2021, Graziella'
-author = 'Graziella'
+project = 'Cires21'
+copyright = '2024, Cires21'
+author = 'Cires21'
 
 release = '0.1'
 version = '0.1.0'
